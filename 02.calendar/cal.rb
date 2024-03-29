4693d1e29c66d54ff614db9621f3ecc4d4dafeb0
sekito <sekito1107gg@gmail.com>
2024-03-29 17:25:01 +0900

    カレンダープログラムを修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,29 +2,28 @@
 require 'date'
 require 'optparse'
 
-#オプションの取得
-options = ARGV.getopts('m:', 'y:').transform_values! {|value| value.to_i unless value == nil}
+options = ARGV.getopts('m:', 'y:').transform_values! {|value| value&.to_i }
 options["m"] ||= Date.today.month
 options["y"] ||= Date.today.year
 
-#月初め、月末のデータを取得
 month_firstday = Date.new(options["y"], options["m"], 1)
 month_lastday = Date.new(options["y"], options["m"], -1)
 firstday_wday = month_firstday.wday
 
-#カレンダーのフォーマットを作成
 puts "      " + "#{month_firstday.month}月 #{month_firstday.year}"
 puts "日 月 火 水 木 金 土"
 print "   " * (firstday_wday)
 
-#日付を表示
 (month_firstday..month_lastday).each do |date|
-  firstday_wday += 1
   if date == Date.today
     print "\e[7m#{date.strftime("%e")}\e[0m"
   else
-  print date.strftime("%e")
+    print date.strftime("%e")
   end
-  date.saturday? ? (puts "") : (print " ")
+  if date.saturday?
+    puts
+  else
+    print(" ")
+  end
 end
-puts ""
+puts
