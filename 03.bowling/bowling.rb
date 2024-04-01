9753f774b6dda63a3438e4948adb545f34951cac
sekito <sekito1107gg@gmail.com>
2024-04-01 17:35:24 +0900

    気になった部分を再度修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -11,27 +11,26 @@ LAST_FLAME = 10
 
 points.each_with_index do |point, index|
   shot += 1
+  # binding.break
   if flame == LAST_FLAME
     score += point
   else
     if point == 10 && shot == 1
-      score += 10 + points[index + 1] + points[index + 2]
+      score += point + points[index + 1] + points[index + 2]
       shot += 1
+      point = 0
     elsif previous_point + point == 10
       score += 10 + points[index + 1]
-    elsif shot == 2
+      point = 0
+      previous_point = 0
+    elsif shot == 1
+      previous_point = point
+    end
+    if shot == 2
       score += previous_point + point
       flame += 1
       shot = 0
       previous_point = 0
-    else
-      previous_point = point
-    end
-    if shot == 2
-      flame += 1
-      shot = 0
-      previous_point = 0
-      next
     end
   end
 end
