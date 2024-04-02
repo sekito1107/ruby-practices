33bff0a3372fae0602d5febbb1d4657630372117
sekito <sekito1107gg@gmail.com>
2024-04-02 15:37:22 +0900

    ロジックの複雑さを解消

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,35 +1,36 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+LAST_FLAME = 10
+
 points = ARGV[0].gsub('X', '10').split(',').map(&:to_i)
 
 score = 0
 shot = 0
 flame = 1
 previous_point = 0
-LAST_FLAME = 10
 
 points.each_with_index do |point, index|
   shot += 1
   if flame == LAST_FLAME
     score += point
   else
-    if point == 10 && shot == 1
-      score += point + points[index + 1] + points[index + 2]
-      shot += 1
-      point = 0
-    elsif previous_point + point == 10
+    strike = point == 10 && shot == 1
+    spare = previous_point + point == 10
+    if strike
+      score += 10 + points[index + 1, 2].sum
+    elsif spare
       score += 10 + points[index + 1]
-      point = 0
-      previous_point = 0
-    elsif shot == 1
-      previous_point = point
-    end
-    if shot == 2
+    elsif shot == 2
       score += previous_point + point
+    end
+
+    if shot == 2 || strike
       flame += 1
       shot = 0
       previous_point = 0
+    else
+      previous_point = point
     end
   end
 end
