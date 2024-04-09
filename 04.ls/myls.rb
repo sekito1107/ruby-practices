a24915b95e572fec4f7d04a86f4f6d89b4ea8d6c
sekito <sekito1107gg@gmail.com>
2024-04-10 01:20:07 +0900

    破壊的メソッドを修正、ARGVへの依存度を低減

--- 04.ls/myls.rb
+++ 04.ls/myls.rb
@@ -4,58 +4,50 @@
 DISPLAY_COLUMNS_COUNT = 3
 
 def run
+  command_line_argument = ARGV
   multiple_arguments_received = false
-  sorted_directories = {}
-  empty_directories = []
-  error_messages = []
-  file_results = []
 
-  main(0, file_results, error_messages, empty_directories, sorted_directories) if ARGV.empty?
+  result_datas = {
+    directory_count: 0,
+    sorted_directories: [],
+    empty_directories: [],
+    error_messages: [],
+    file_results: []
+  }
 
-  ARGV.size.times do |index|
-    main(index, file_results, error_messages, empty_directories, sorted_directories)
-  end
+  result_datas = create_result_datas(command_line_argument, result_datas)
 
-  multiple_arguments_received = true if ARGV.size >= 2
-  result_display(file_results, empty_directories, error_messages, sorted_directories, multiple_arguments_received)
+  multiple_arguments_received = true if command_line_argument.size >= 2
+  result_display(result_datas, multiple_arguments_received)
 end
 
-def main(index, file_results, error_messages, empty_directories, sorted_directories)
-  data_type = :directory_path
-  data_type = command_line_argument_type(ARGV[index]) if ARGV[index]
-  sorted_directories[:directory_count] ||= 0
-  sorted_directories[:directory_name] ||= []
-  case data_type
-  when :invalid
-    error_messages << create_error_messages(index)
-  when :file_path
-    file_results << create_display_file_results(ARGV[index])
-  when :directory_path
-    directory_files = create_directory_file(index, sorted_directories, empty_directories)
-    sorted_directories[:rows_count] ||= []
-    sorted_directories[:rows_count] << calculate_row_count(directory_files)
-    sorted_directories[:formatted_datas] ||= []
-    sorted_directories[:formatted_datas] << create_formatted_data(directory_files, sorted_directories)
-  end
-end
-
-def create_directory_file(index, sorted_directories, empty_directories)
-  directory_files = if ARGV[index]
-                      Dir.glob("#{ARGV[index]}/*").map { File.basename(_1) }
-                    else
-                      Dir.glob('*')
-                    end
-  if directory_files.empty?
-    empty_directories << ARGV[index].to_s
+def create_result_datas(command_line_argument, result_datas)
+  if command_line_argument.empty?
+    result_datas[:directory_count] += 1
+    result_datas[:sorted_directories] << create_sorted_directory(0, nil)
   else
-    sorted_directories[:directory_count] += 1
-    sorted_directories[:directory_name] << ARGV[index]
+    command_line_argument.size.times do |index|
+      data_type = command_line_argument_type(command_line_argument[index])
+      case data_type
+      when :directory_path
+        result_datas[:directory_count] += 1
+        result_datas[:sorted_directories] << create_sorted_directory(index, command_line_argument)
+      when :file_path
+        result_datas[:file_results] << create_display_file_result(command_line_argument[index])
+      when :empty_directory
+        result_datas[:empty_directories] << command_line_argument[index]
+      when :invalid
+        result_datas[:error_messages] << create_error_message(index, command_line_argument)
+      end
+    end
   end
-  directory_files
+  result_datas
 end
 
 def command_line_argument_type(command_line_argument_data)
   if File.directory?(command_line_argument_data)
+    return :empty_directory if Dir.glob("#{command_line_argument_data}/*").empty?
+
     :directory_path
   elsif File.file?(command_line_argument_data)
     :file_path
@@ -64,63 +56,80 @@ def command_line_argument_type(command_line_argument_data)
   end
 end
 
-def create_error_messages(index)
-  "ls: '#{ARGV[index]}' にアクセスできません：そのようなファイルやディレクトリはありません"
+def create_sorted_directory(index, command_line_argument)
+  if command_line_argument
+    directory_files = Dir.glob("#{command_line_argument[index]}/*").map { File.basename(_1) }
+    directory_name = command_line_argument[index]
+  else
+    directory_files = Dir.glob('*')
+  end
+  row_count = calculate_row_count(directory_files)
+  formatted_datas = create_formatted_data(directory_files, row_count)
+
+  {
+    directory_name:,
+    row_count:,
+    formatted_datas:
+  }
 end
 
-def create_display_file_results(file_path)
+def create_display_file_result(file_path)
   file_path == '~' ? File.expand_path(file_path) : file_path
 end
 
-def calculate_row_count(directory_files)
-  rows_count = (directory_files.size + DISPLAY_COLUMNS_COUNT - 1) / DISPLAY_COLUMNS_COUNT
-  rows_count.zero? ? 1 : rows_count
+def create_error_message(index, command_line_argument)
+  "ls: '#{command_line_argument[index]}' にアクセスできません：そのようなファイルやディレクトリはありません"
 end
 
-def create_formatted_data(directory_files, sorted_directories)
-  row_count = sorted_directories[:rows_count][sorted_directories[:directory_count] - 1]
+def calculate_row_count(directory_files)
+  row_count = (directory_files.size + DISPLAY_COLUMNS_COUNT - 1) / DISPLAY_COLUMNS_COUNT
+  row_count.zero? ? 1 : row_count
+end
+
+def create_formatted_data(directory_files, row_count)
   formatted_data = directory_files.each_slice(row_count).to_a
   formatted_data << [''] while formatted_data.size < DISPLAY_COLUMNS_COUNT
   formatted_data
 end
 
-def result_display(file_results, empty_directories, error_messages, sorted_directories, multiple_arguments_received)
-  display_error_messages(error_messages) unless error_messages.empty?
-  display_file_results(file_results, empty_directories, sorted_directories) unless file_results.empty?
-  display_empty_message(empty_directories, sorted_directories, multiple_arguments_received) unless empty_directories.empty?
-  display_directory_results(sorted_directories, multiple_arguments_received) unless sorted_directories.empty?
+def result_display(result_datas, multiple_arguments_received)
+  display_error_messages(result_datas) unless result_datas[:error_messages].empty?
+  display_file_results(result_datas) unless result_datas[:file_results].empty?
+  display_empty_message(result_datas, multiple_arguments_received) unless result_datas[:empty_directories].empty?
+  display_directory_results(result_datas, multiple_arguments_received) unless result_datas[:sorted_directories].empty?
 end
 
-def display_error_messages(error_messages)
-  error_messages.each { puts _1 }
+def display_error_messages(result_datas)
+  result_datas[:error_messages].each { puts _1 }
 end
 
-def display_file_results(file_results, empty_directories, sorted_directories)
-  file_results.sort.each { print "#{_1}  " }
+def display_file_results(result_datas)
+  result_datas[:file_results].sort.each { print "#{_1}  " }
   puts
-  puts if !sorted_directories.empty? || !empty_directories.empty?
+  puts if !result_datas[:sorted_directories].empty? || !result_datas[:empty_directories].empty?
 end
 
-def display_empty_message(empty_directories, sorted_directories, multiple_arguments_received)
+def display_empty_message(result_datas, multiple_arguments_received)
   unless multiple_arguments_received
     print ''
     exit
   end
-  empty_directories.each.with_index do |directory_name, empty_index|
+  result_datas[:empty_directories].each.with_index do |directory_name, empty_index|
     puts "#{directory_name}:"
-    puts unless empty_directories.size - 1 == empty_index || sorted_directories.empty?
+    puts if result_datas[:empty_directories].size - 1 != empty_index || !result_datas[:sorted_directories].empty?
   end
 end
 
-def display_directory_results(sorted_directories, multiple_arguments_received)
-  sorted_directories[:directory_count].times do |directory_number|
+def display_directory_results(result_datas, multiple_arguments_received)
+  result_datas[:directory_count].times do |directory_number|
+    target_directory_data = result_datas[:sorted_directories][directory_number]
     puts if directory_number.positive? && multiple_arguments_received
-    puts "#{sorted_directories[:directory_name][directory_number]}:" if multiple_arguments_received
-    max_column_widths = calculate_max_column_widths(sorted_directories[:formatted_datas][directory_number])
-    sorted_directories[:rows_count][directory_number].times do |row|
+    puts "#{target_directory_data[:directory_name]}:" if multiple_arguments_received
+    max_column_widths = calculate_max_column_widths(target_directory_data[:formatted_datas])
+    target_directory_data[:row_count].times do |row|
       DISPLAY_COLUMNS_COUNT.times do |col|
-        wide_chars_count = count_multibyte_characters(sorted_directories[:formatted_datas][directory_number][col][row]) || 0
-        print sorted_directories[:formatted_datas][directory_number][col][row].to_s.ljust((max_column_widths[col] || 0) + 2 - wide_chars_count)
+        wide_chars_count = count_multibyte_characters(target_directory_data[:formatted_datas][col][row]) || 0
+        print target_directory_data[:formatted_datas][col][row].to_s.ljust((max_column_widths[col] || 0) + 2 - wide_chars_count)
       end
       puts
     end
