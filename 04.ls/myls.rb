b6e4128f177fb5927cbd672bdd7be17afd652a49
sekito <sekito1107gg@gmail.com>
2024-04-23 15:56:38 +0900

    -rオプションを実装

--- 04.ls/myls.rb
+++ 04.ls/myls.rb
@@ -8,29 +8,34 @@ DISPLAY_COLUMNS_COUNT = 3
 def run
   options = fetch_options!(ARGV)
   selected_files = ARGV.empty? ? ['.'] : ARGV
+  sorted_selected_files = sort_selected_files(selected_files, options[:r])
 
-  result_data = create_result_data(selected_files, options)
+  result_data = create_result_data(sorted_selected_files, options)
 
   result_display(result_data, selected_files.size >= 2)
 end
 
 def fetch_options!(command_line_argument)
   options = {
-    a: false
+    r: false
   }
   opt = OptionParser.new
-  opt.on('-a') { options[:a] = true }
+  opt.on('-r') { options[:r] = true }
   opt.parse!(command_line_argument)
   options
 end
 
+def sort_selected_files(selected_files, opt_r)
+  opt_r ? selected_files.sort.reverse : selected_files.sort
+end
+
 def create_result_data(selected_files, options)
   result_data = initialize_result_data
-  selected_files.sort.each do |filenames|
+  selected_files.each do |filenames|
     data_type = file_type(filenames)
     case data_type
     when :directory_path
-      result_data[:sorted_directories] << create_sorted_directory(filenames, options[:a])
+      result_data[:sorted_directories] << create_sorted_directory(filenames, options[:r])
     when :file_path
       result_data[:file_results] << filenames
     when :invalid
@@ -59,14 +64,14 @@ def file_type(filenames)
   end
 end
 
-def create_sorted_directory(filenames, opt_a)
-  glob_flags = opt_a ? File::FNM_DOTMATCH : 0
-  directory_files = Dir.glob("#{filenames}/*", glob_flags).map { File.basename(_1) }
+def create_sorted_directory(filenames, opt_r)
+  directory_files = Dir.glob("#{filenames}/*").map { File.basename(_1) }
+  riverse_directory_files = directory_files.reverse if opt_r
 
   directory_name = filenames
 
   row_count = calculate_row_count(directory_files)
-  formatted_data = create_formatted_data(directory_files, row_count)
+  formatted_data = create_formatted_data(riverse_directory_files || directory_files, row_count)
 
   {
     directory_name:,
