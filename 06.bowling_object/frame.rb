bbc446c0c7a760140049ad316e78a426fb5d5f0a
sekito <sekito1107gg@gmail.com>
2024-05-31 23:21:35 +0900

    ボウリングのスコア計算プログラムを作成

---
+++ 06.bowling_object/frame.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class Frame
+  def initialize(frame_number)
+    @frame_number = frame_number
+    @score = 0
+    @bonus = 0
+    @shot_count = 0
+    @bonus_count = 0
+  end
+
+  def record_shot(pins)
+    @score += pins
+    @shot_count += 1
+  end
+
+  def strike?
+    @shot_count == 1 && @score == 10
+  end
+
+  def spare?
+    @shot_count == 2 && @score == 10
+  end
+
+  def finished?
+    @shot_count == 2 || @score == 10
+  end
+
+  def score
+    @score + @bonus
+  end
+
+  def need_bonus?
+    if strike?
+      @bonus_count < 2
+    elsif spare?
+      @bonus_count < 1
+    end
+  end
+
+  def add_bonus(pins)
+    @bonus += pins
+    @bonus_count += 1
+  end
+
+  def last_frame?
+    @frame_number == 10
+  end
+end
---
+++ 06.bowling_object/game.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative 'frame'
+
+class Game
+  attr_reader :score_board
+
+  def initialize
+    @frames = [Frame.new(1)]
+    @special_frames = []
+    @score_board = ARGV[0].gsub('X', '10').split(',').map(&:to_i)
+  end
+
+  def record_shot(pins)
+    calc_bonus(pins)
+    frame = @frames.last
+    frame.record_shot(pins)
+    return if frame.last_frame?
+
+    @frames << Frame.new(@frames.size + 1) if frame.finished?
+    @special_frames << frame if frame.strike? || frame.spare?
+  end
+
+  def calc_bonus(pins)
+    @special_frames.each do |special_frame|
+      special_frame.add_bonus(pins) if special_frame.need_bonus?
+    end
+  end
+
+  def score
+    result = 0
+    @frames.each do |frame|
+      result += frame.score
+    end
+    result
+  end
+end
+
+game = Game.new
+game.score_board.each do |shot_pins|
+  game.record_shot(shot_pins)
+end
+puts game.score
